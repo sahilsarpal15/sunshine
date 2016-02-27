856ecc95dd86688d99581e7f059bffebce422eaa
Sahil <sahilsarpal@gmail.com>
2016-02-27 19:18:16 +0530

    lesson 2 completed

---
+++ app/src/main/java/sunshine/ambidextrous/com/sunshine/WeatherDataParser.java
@@ -0,0 +1,16 @@
+package sunshine.ambidextrous.com.sunshine;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by sahil on 27/2/16.
+ */
+public class WeatherDataParser {
+//    public static double getMaxTemperatureForDay(String weatherJsonStr, int dayIn) {
+//        JSONObject weather = new JSONObject(weatherJsonStr);
+//        JSONArray days = weather.getJSONArray("list");
+//        JSONObject dayInfo = days.getJSONObject("temp");
+//        return temperatureInfo.getDouble("max");
+//    }
+}
